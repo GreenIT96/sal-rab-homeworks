75b2a64a2fedc6c12ca10cc098c751b86e8cb079
GreenIT96 <zelensky.ant@gmail.com>
2023-03-04 21:06:31 +0500

    final_task_1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,26 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: name + ' ' + phone, goods: [], order: {address: 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв. ' + address.flat, sum: sum}}
+    console.log(data);
+    
     let countOfGoods = goods.length;
+    
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i <= countOfGoods; i++) {
+        //data.goods.push(goods[i].title);
         data.goods.push(goods[i].title);
+
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    //data.order.address = address;
+    //data.order.sum = name + phone + address + goods + sum;
 
-    data.client = 'Иван';
+    //data.client = 'Иван';
 
     let jsonData = JSON.stringify(data);
+    
 
     return jsonData;
 }
+
